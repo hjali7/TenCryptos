fa21255d19946d35fe1b079b5865ab1370f53cd7
hjali7 <hjikingali@gmail.com>
2025-04-14 00:51:00 +0330

    added healthcheck and volumes to docker compose

--- frontend/src/lib/api.ts
+++ frontend/src/lib/api.ts
@@ -1,12 +1,11 @@
 export async function fetchCryptos() {
-    const res = await fetch("http://127.0.0.1:8000/cryptos", {
-      next: { revalidate: 0 },
-    });
-  
-    if (!res.ok) {
-      throw new Error("Failed to fetch cryptos");
-    }
-  
-    return res.json();
+  const res = await fetch("/api/cryptos", {
+    next: { revalidate: 0 },
+  });
+
+  if (!res.ok) {
+    throw new Error("Failed to fetch cryptos");
   }
-  
+
+  return res.json();
+}
