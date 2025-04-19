7db19dfc256a2dec67c35b2829a3be21d0e57dde
hjali7 <hjikingali@gmail.com>
2025-04-19 19:03:48 +0330

    bugs fixed and automation for data triger

--- infrastructure/go_lambdas/init_updater/main.go
+++ infrastructure/go_lambdas/init_updater/main.go
@@ -3,31 +3,19 @@ package main
 import (
 	"bytes"
 	"fmt"
-	"io"
 	"net/http"
-
-	"github.com/aws/aws-lambda-go/events"
-	"github.com/aws/aws-lambda-go/lambda"
 )
 
-func handler(event events.SQSEvent) error {
-	for _, record := range event.Records {
-		fmt.Printf("📥 Received message: %s\n", record.Body)
-
-		// 1. ارسال درخواست به بک‌اند
-		url := "http://host.docker.internal:8000/cryptos/update"
-		resp, err := http.Post(url, "application/json", bytes.NewBuffer([]byte("{}")))
-		if err != nil {
-			fmt.Println("❌ Failed to call update API:", err)
-			continue
-		}
-		defer resp.Body.Close()
-		body, _ := io.ReadAll(resp.Body)
-		fmt.Println("✅ API Response:", string(body))
-	}
-	return nil
-}
-
 func main() {
-	lambda.Start(handler)
+	url := "http://backend:8000/cryptos/update"
+	payload := []byte(`{}`)
+
+	resp, err := http.Post(url, "application/json", bytes.NewBuffer(payload))
+	if err != nil {
+		fmt.Println("❌ Failed to call backend:", err)
+		return
+	}
+	defer resp.Body.Close()
+
+	fmt.Println("✅ Called /cryptos/update, status:", resp.Status)
 }
